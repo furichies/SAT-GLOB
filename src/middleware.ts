d89e66a189f523b4aaed01d87c7853c42b656af0
furichies <furichies@yandex.com>
2026-04-03 23:24:16 +0200

    fix: middleware checks role server-side

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,22 @@
 import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
-export default withAuth({
-  pages: {
-    signIn: '/auth/login',
+export default withAuth(
+  function middleware(req) {
+    const role = req.nextauth.token?.role as string
+    
+    if (role !== 'admin' && role !== 'superadmin' && role !== 'tecnico') {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
+    
+    return NextResponse.next()
   },
-})
+  {
+    pages: {
+      signIn: '/auth/login',
+    },
+  }
+)
 
 export const config = {
   matcher: [
