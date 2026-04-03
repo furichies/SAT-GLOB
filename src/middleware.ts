b91abe4f2f2f13c00d8819232e032bed65c1d2e4
furichies <furichies@yandex.com>
2026-04-03 23:23:20 +0200

    fix: use withAuth middleware for /admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,6 @@
 import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token,
-  },
   pages: {
     signIn: '/auth/login',
   },
